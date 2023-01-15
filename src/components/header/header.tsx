6d0f29ab0d311dc01f6103136e04526408744f5b
mahmoudtarek <mahmoudtarek@code.bld.ai>
2023-01-15 17:58:06 +0200

    chore: making the menu responsive on mobile devices

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,39 +1,77 @@
+import { useEffect, useState } from "react";
+
 //components
 import AppBar from "@mui/material/AppBar";
 import Typography from "@mui/material/Typography";
 import { StyledToolbar, StyledNav } from "./styles";
 import { Link } from "react-router-dom";
+import { useTheme } from "@mui/material/styles";
+
+//utils
+import { useMediaQuery } from "@mui/material";
 
 //icons
 import MenuIcon from "@icons/Menu";
+import CloseIcon from "@icons/Close";
 
 export default function Header() {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(() => theme.breakpoints.down("md"));
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = (open: boolean) => () => {
+    setOpen(open);
+  };
+
+  const nav = (
+    <StyledNav>
+      <ul>
+        <Link to="/characters" onClick={handleOpen(false)}>
+          <Typography component="li" fontWeight={700} variant="h3">
+            Characters
+          </Typography>
+        </Link>
+        <Link to="/locations" onClick={handleOpen(false)}>
+          <Typography component="li" fontWeight={700} variant="h3">
+            Locations
+          </Typography>
+        </Link>
+        <Link to="/episodes" onClick={handleOpen(false)}>
+          <Typography component="li" fontWeight={700} variant="h3">
+            Episodes
+          </Typography>
+        </Link>
+      </ul>
+    </StyledNav>
+  );
+
+  useEffect(() => {
+    if (open) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [open]);
+
   return (
     <AppBar color="transparent" position="relative">
       <StyledToolbar>
         <img src={import.meta.env.BASE_URL + "logo.svg"} alt="logo" />
-        <StyledNav>
-          <ul>
-            <Link to="/characters">
-              <Typography component="li" fontWeight={700} variant="h3">
-                Characters
-              </Typography>
-            </Link>
-            <Link to="/locations">
-              <Typography component="li" fontWeight={700} variant="h3">
-                Locations
-              </Typography>
-            </Link>
-            <Link to="/episodes">
-              <Typography component="li" fontWeight={700} variant="h3">
-                Episodes
-              </Typography>
-            </Link>
-          </ul>
-        </StyledNav>
-        <span className="MenuIcon">
-          <MenuIcon />
-        </span>
+        {isMobile ? <></> : nav}
+        {!open ? (
+          <span className="MenuIcon" onClick={handleOpen(true)}>
+            <MenuIcon />
+          </span>
+        ) : (
+          <span className="MenuIcon" onClick={handleOpen(false)}>
+            <CloseIcon />
+          </span>
+        )}
+        <div className={(open ? "show" : "") + " mobile-nav"}>{nav}</div>
       </StyledToolbar>
     </AppBar>
   );
--- src/components/header/styles.ts
+++ src/components/header/styles.ts
@@ -22,9 +22,39 @@ export const StyledToolbar = styled(Toolbar)(({ theme }) => ({
     paddingLeft: "10px !important",
     paddingRight: "10px !important",
   },
+  [theme.breakpoints.up("md")]: {
+    "& .mobile-nav": {
+      display: "none",
+      "& ul": {
+        display: "none",
+      },
+    },
+  },
+  "& .mobile-nav": {
+    "& nav": {
+      width: "100%",
+    },
+    "& ul": {
+      flexDirection: "column",
+      gap: 48,
+      marginTop: 48,
+    },
+    width: "100%",
+    height: "100vh",
+    backgroundColor: "white",
+    position: "fixed",
+    inset: 0,
+    top: 61,
+    zIndex: theme.zIndex.appBar * 2,
+    display: "none",
+    "&.show": {
+      display: "flex",
+    },
+  },
 }));
 
 export const StyledNav = styled("nav")(({ theme }) => ({
+  height: "fit-content",
   "& ul": {
     padding: 0,
     display: "flex",
@@ -36,10 +66,5 @@ export const StyledNav = styled("nav")(({ theme }) => ({
     fontSize: "18px",
     lineHeight: "21px",
     fontFamily: "'Karla', sans-serif",
-  },
-  [theme.breakpoints.down("md")]: {
-    "& ul": {
-      display: "none",
-    },
   },
 }));
